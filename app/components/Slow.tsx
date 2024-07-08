c48ef79b83e1e1f545147ed1a4c3a47dc8227e3a
blue.zeng <blue.zeng@mirmiles.com>
2024-07-08 13:00:34 +0800

    fix: log

--- app/components/Slow.tsx
+++ app/components/Slow.tsx
@@ -4,6 +4,6 @@ interface SlowProps {
 }
 
 export default async function Slow({ time, text }: SlowProps) {
-  await new Promise((resolve) => setTimeout(resolve, time));
-  return <div className={`w-1/2 rounded-md p-10 m-1 bg-red-300`}>{text}</div>;
+  // await new Promise((resolve) => setTimeout(resolve, time));
+  return <div className={`w-full rounded-md p-10 m-1 bg-red-300`}>{text}</div>;
 }
--- app/user-home/page.tsx
+++ app/user-home/page.tsx
@@ -1,25 +1,24 @@
-import { Suspense } from "react";
-import Slow from "../components/Slow";
-import UserList from "../components/UserList";
+import { Suspense } from "react"
+import Slow from "../components/Slow"
+import UserList from "../components/UserList"
 
-export const revalidate = 0; // 禁用缓存
-
+export const revalidate = 0 // 禁用缓存
 
 export default async function HomePage() {
-  console.log("服务端渲染 ...");
+  console.log("服务端渲染 ...")
   return (
     <div className="p-5">
       <h1>用户列表</h1>
       <UserList />
-      <h1>模拟耗时组件</h1>
-      <div className="flex ">
+      <h1 className="mt-5">模拟耗时组件</h1>
+      <div>
         {/* <Suspense fallback={"Loading..."}> */}
-          <Slow time={3000} text={"有点慢"} />
+        <Slow time={3000} text={"有点慢"} />
         {/* </Suspense> */}
         {/* <Suspense fallback={"Loading..."}> */}
-          <Slow time={5000} text={"更慢"} />
+        <Slow time={5000} text={"更慢"} />
         {/* </Suspense> */}
       </div>
     </div>
-  );
+  )
 }
